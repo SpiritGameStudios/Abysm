af01cb5f5aba883bcef61a6955e83a4ac1be6613
Kat <superkat1806@gmail.com>
2025-05-25 02:30:23 -0400

    Scabiosa datagen-able - thanks IntelliJ for removing my selected stuff

---
+++ src/client/java/dev/spiritstudios/abysm/datagen/ClientBlockMetatagProvider.java
@@ -0,0 +1,29 @@
+package dev.spiritstudios.abysm.datagen;
+
+import dev.spiritstudios.abysm.registry.AbysmBlocks;
+import dev.spiritstudios.specter.api.registry.metatag.datagen.MetatagProvider;
+import dev.spiritstudios.specter.api.render.client.BlockRenderLayer;
+import dev.spiritstudios.specter.api.render.client.RenderMetatags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.block.Block;
+import net.minecraft.data.DataOutput;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public class ClientBlockMetatagProvider extends MetatagProvider<Block> {
+	protected ClientBlockMetatagProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+		super(dataOutput, RegistryKeys.BLOCK, registriesFuture, DataOutput.OutputType.RESOURCE_PACK);
+	}
+
+	@Override
+	protected void configure(Consumer<MetatagBuilder<Block, ?>> consumer, RegistryWrapper.WrapperLookup wrapperLookup) {
+		MetatagBuilder<Block, BlockRenderLayer> renderLayer = create(RenderMetatags.RENDER_LAYER);
+
+		renderLayer.put(AbysmBlocks.PURPLE_SCABIOSA, BlockRenderLayer.CUTOUT);
+
+		consumer.accept(renderLayer);
+	}
+}
---
+++ src/main/java/dev/spiritstudios/abysm/block/ScabiosaBlock.java
@@ -0,0 +1,94 @@
+package dev.spiritstudios.abysm.block;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
+import org.jetbrains.annotations.Nullable;
+
+public class ScabiosaBlock extends Block {
+	public static final MapCodec<ScabiosaBlock> CODEC = createCodec(ScabiosaBlock::new);
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+	public static final EnumProperty<Direction> FACING = Properties.FACING;
+
+	@Override
+	protected MapCodec<? extends Block> getCodec() {
+		return CODEC;
+	}
+
+	public ScabiosaBlock(Settings settings) {
+		super(settings);
+		this.setDefaultState(this.getDefaultState().with(WATERLOGGED, false).with(FACING, Direction.UP));
+	}
+
+	// Pretty much all copy-pasted from AmethystClusterBlock
+	@Override
+	protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+		Direction direction = state.get(FACING);
+		BlockPos blockPos = pos.offset(direction.getOpposite());
+		return world.getBlockState(blockPos).isSideSolidFullSquare(world, blockPos, direction);
+	}
+
+	@Override
+	protected BlockState getStateForNeighborUpdate(
+		BlockState state,
+		WorldView world,
+		ScheduledTickView tickView,
+		BlockPos pos,
+		Direction direction,
+		BlockPos neighborPos,
+		BlockState neighborState,
+		Random random
+	) {
+		if (state.get(WATERLOGGED)) {
+			tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+
+		return direction == state.get(FACING).getOpposite() && !state.canPlaceAt(world, pos)
+			? Blocks.AIR.getDefaultState()
+			: super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+	}
+
+	@Nullable
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		WorldAccess worldAccess = ctx.getWorld();
+		BlockPos blockPos = ctx.getBlockPos();
+		return this.getDefaultState().with(WATERLOGGED, worldAccess.getFluidState(blockPos).getFluid() == Fluids.WATER).with(FACING, ctx.getSide());
+	}
+
+	@Override
+	protected BlockState rotate(BlockState state, BlockRotation rotation) {
+		return state.with(FACING, rotation.rotate(state.get(FACING)));
+	}
+
+	@Override
+	protected BlockState mirror(BlockState state, BlockMirror mirror) {
+		return state.rotate(mirror.getRotation(state.get(FACING)));
+	}
+
+	@Override
+	protected FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(WATERLOGGED, FACING);
+	}
+}
