823503c7b2918756c7fa5d81652428cf16cb16df
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-06-13 17:30:10 -0700

    Fix: specter has a multipart API
    I am sad now

--- src/main/java/dev/spiritstudios/abysm/entity/leviathan/Leviathan.java
+++ src/main/java/dev/spiritstudios/abysm/entity/leviathan/Leviathan.java
@@ -1,29 +1,26 @@
 package dev.spiritstudios.abysm.entity.leviathan;
 
+import dev.spiritstudios.specter.api.entity.PartHolder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.boss.dragon.EnderDragonPart;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.Monster;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.Box;
 import net.minecraft.world.World;
 
-import java.util.List;
-
-public abstract class Leviathan extends MobEntity implements Monster {
+public abstract class Leviathan<L extends Leviathan<L>> extends MobEntity implements Monster, PartHolder<Leviathan<L>> {
 
 	protected Leviathan(EntityType<? extends MobEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 	@SuppressWarnings("unused")
-	public boolean damagePart(ServerWorld world, LeviathanPart part, DamageSource source, float amount) {
+	public boolean damagePart(ServerWorld world, LeviathanPart<L> part, DamageSource source, float amount) {
 		return this.damage(world, source, amount);
 	}
 
@@ -43,8 +40,6 @@ public abstract class Leviathan extends MobEntity implements Monster {
 		super.heal(Math.max(0, amount));
 	}
 
-	public abstract List<LeviathanPart> getParts();
-
 	@Override
 	protected boolean canStartRiding(Entity entity) {
 		return false;
@@ -58,7 +53,7 @@ public abstract class Leviathan extends MobEntity implements Monster {
 	@Override
 	public void onSpawnPacket(EntitySpawnS2CPacket packet) {
 		super.onSpawnPacket(packet);
-		List<LeviathanPart> parts = this.getParts();
+		var parts = this.parts();
 
 		for(int i = 0; i < parts.size(); ++i) {
 			parts.get(i).setId(i + packet.getEntityId() + 1);
--- src/main/java/dev/spiritstudios/abysm/entity/leviathan/LeviathanPart.java
+++ src/main/java/dev/spiritstudios/abysm/entity/leviathan/LeviathanPart.java
@@ -1,77 +1,22 @@
 package dev.spiritstudios.abysm.entity.leviathan;
 
-import net.minecraft.entity.Entity;
+import dev.spiritstudios.specter.api.entity.EntityPart;
 import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.data.DataTracker;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.network.listener.ClientPlayPacketListener;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.server.network.EntityTrackerEntry;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.Nullable;
 
-public class LeviathanPart extends Entity {
+public class LeviathanPart<T extends Leviathan<T>> extends EntityPart<T> {
 
-	public final Leviathan owner;
 	public final String name;
-	private final EntityDimensions partDimensions;
 
-	public LeviathanPart(Leviathan owner, String name, float width, float height) {
-		super(owner.getType(), owner.getWorld());
-		this.partDimensions = EntityDimensions.changing(width, height);
-		this.calculateDimensions();
-		this.owner = owner;
+	public LeviathanPart(T owner, String name, float width, float height) {
+		super(owner, EntityDimensions.changing(width, height));
 		this.name = name;
 	}
 
-	@Override
-	protected void initDataTracker(DataTracker.Builder builder) {
-	}
-
-	@Override
-	protected void readCustomDataFromNbt(NbtCompound nbt) {
-	}
-
-	@Override
-	protected void writeCustomDataToNbt(NbtCompound nbt) {
-	}
-
-	@Override
-	public boolean canHit() {
-		return true;
-	}
-
-	@Nullable
-	@Override
-	public ItemStack getPickBlockStack() {
-		return this.owner.getPickBlockStack();
-	}
-
+	@SuppressWarnings("SimplifiableConditionalExpression")
 	@Override
 	public final boolean damage(ServerWorld world, DamageSource source, float amount) {
-		return this.isAlwaysInvulnerableTo(source) ? false : this.owner.damagePart(world, this, source, amount);
-	}
-
-	@Override
-	public boolean isPartOf(Entity entity) {
-		return this == entity || this.owner == entity;
-	}
-
-	@Override
-	public Packet<ClientPlayPacketListener> createSpawnPacket(EntityTrackerEntry entityTrackerEntry) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public EntityDimensions getDimensions(EntityPose pose) {
-		return this.partDimensions;
-	}
-
-	@Override
-	public boolean shouldSave() {
-		return false;
+		return this.isAlwaysInvulnerableTo(source) ? false : this.getOwner().damagePart(world, this, source, amount);
 	}
 }
