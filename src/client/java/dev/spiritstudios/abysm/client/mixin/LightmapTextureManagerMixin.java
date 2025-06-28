1d449be10e4fd7a63eaf3b1d2bac05fed903cf8d
Phanastrae <111198528+Phanastrae@users.noreply.github.com>
2025-06-28 08:15:49 +0100

    feat: restore lightmap adjustments

--- src/client/java/dev/spiritstudios/abysm/client/mixin/LightmapTextureManagerMixin.java
+++ src/client/java/dev/spiritstudios/abysm/client/mixin/LightmapTextureManagerMixin.java
@@ -1,39 +1,52 @@
 package dev.spiritstudios.abysm.client.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.systems.RenderPass;
-import dev.spiritstudios.abysm.client.duck.ClientPlayerEntityDuckInterface;
+import com.mojang.blaze3d.systems.GpuDevice;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.textures.FilterMode;
+import com.mojang.blaze3d.textures.GpuTexture;
+import com.mojang.blaze3d.textures.TextureFormat;
+import dev.spiritstudios.abysm.client.render.LightmapAdjustment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.registry.tag.FluidTags;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
 
 @Mixin(LightmapTextureManager.class)
 public abstract class LightmapTextureManagerMixin {
 	@Shadow @Final private MinecraftClient client;
+	@Shadow @Final private GpuTexture glTexture;
 
-	@WrapOperation(method = "update", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderPass;setUniform(Ljava/lang/String;[F)V", ordinal = 1))
-	private void tweakLightmap(RenderPass instance, String s, float[] floats, Operation<Void> original, @Local(argsOnly = true) float tickProgress) {
+	@Unique private GpuTexture secondaryGlTexture;
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void initDummy(GameRenderer renderer, MinecraftClient client, CallbackInfo ci) {
+		GpuDevice gpuDevice = RenderSystem.getDevice();
+		this.secondaryGlTexture = gpuDevice.createTexture("Light Texture", TextureFormat.RGBA8, 16, 16, 1);
+		this.secondaryGlTexture.setTextureFilter(FilterMode.LINEAR, false);
+		gpuDevice.createCommandEncoder().clearColorTexture(this.secondaryGlTexture, -1);
+	}
+
+	@Inject(method = "close", at = @At("RETURN"))
+	private void closeDummy(CallbackInfo ci) {
+		this.secondaryGlTexture.close();
+	}
+
+	@Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V"))
+	private void tweakLightmap(float tickProgress, CallbackInfo ci) {
 		ClientPlayerEntity player = this.client.player;
 
-		// these should not be null at this point
+		// player should not be null
 		Objects.requireNonNull(player);
 
-		float brightenSkyFactor = 0.0F;
-
-		if (player.isSubmergedIn(FluidTags.WATER)) {
-			brightenSkyFactor = ((ClientPlayerEntityDuckInterface) player).abysm$getUnderwaterAmbientSkyLight(tickProgress);
-		}
-
-		floats[0] += brightenSkyFactor;
-		original.call(instance, s, floats);
+		LightmapAdjustment.adjustLightmap(player, this.glTexture, this.secondaryGlTexture, tickProgress);
 	}
 }
