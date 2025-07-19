c059c72e6394b0c79987ed8d35f53b35bc320e6e
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-07-19 00:58:22 -0700

    Fix: restore PlayerEntityRendererMixin
    Better this way for compat (I think)

--- src/client/java/dev/spiritstudios/abysm/client/mixin/harpoon/PlayerEntityRendererMixin.java
+++ src/client/java/dev/spiritstudios/abysm/client/mixin/harpoon/PlayerEntityRendererMixin.java
@@ -1,21 +1,24 @@
 package dev.spiritstudios.abysm.client.mixin.harpoon;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.spiritstudios.abysm.item.AbysmItems;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerEntityRenderer.class)
 public abstract class PlayerEntityRendererMixin {
-	@WrapMethod(method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;")
-	private static BipedEntityModel.ArmPose harpoonPose(PlayerEntity player, ItemStack stack, Hand hand, Operation<BipedEntityModel.ArmPose> original) {
-		return !player.handSwinging && stack.isOf(AbysmItems.HARPOON) ?
-			BipedEntityModel.ArmPose.CROSSBOW_HOLD :
-			original.call(player, stack, hand);
+
+	@WrapOperation(method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+	private static boolean holdHarpoon(ItemStack instance, Item item, Operation<Boolean> original) {
+		return original.call(instance, item) || original.call(instance, AbysmItems.HARPOON);
+	}
+
+	@WrapOperation(method = "getArmPose(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/Hand;)Lnet/minecraft/client/render/entity/model/BipedEntityModel$ArmPose;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;isCharged(Lnet/minecraft/item/ItemStack;)Z"))
+	private static boolean harpoonAlwaysCharged(ItemStack stack, Operation<Boolean> original) {
+		return stack.isOf(AbysmItems.HARPOON) || original.call(stack);
 	}
 }
