4989155a8ecaf724ac11419b9fa6846759faab0a
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-08-01 19:01:55 +1000

    chore(worldgen): fix a small issue with structurepools

--- src/main/java/dev/spiritstudios/abysm/worldgen/structure/pool/AbysmStructurePools.java
+++ src/main/java/dev/spiritstudios/abysm/worldgen/structure/pool/AbysmStructurePools.java
@@ -3,18 +3,17 @@ package dev.spiritstudios.abysm.worldgen.structure.pool;
 import com.google.common.collect.ImmutableList;
 import com.mojang.datafixers.util.Pair;
 import dev.spiritstudios.abysm.Abysm;
+import dev.spiritstudios.specter.api.worldgen.SpecterStructurePoolElements;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.structure.pool.StructurePool;
-import net.minecraft.structure.pool.StructurePoolElement;
 import net.minecraft.structure.pool.StructurePools;
 import net.minecraft.structure.processor.StructureProcessorList;
 
 public class AbysmStructurePools {
-
 	public static final RegistryKey<StructurePool> BASIC_RUIN = of("deep_sea_ruins/basic_ruin");
 
 	public static void bootstrap(Registerable<StructurePool> registerable) {
@@ -28,7 +27,7 @@ public class AbysmStructurePools {
 			new StructurePool(
 				emptyPool,
 				ImmutableList.of(Pair.of(
-					StructurePoolElement.ofSingle(Abysm.id("deep_sea_ruins/basic_ruin").toString()),
+					SpecterStructurePoolElements.ofSingle(Abysm.id("deep_sea_ruins/basic_ruin")),
 					1
 				)),
 				StructurePool.Projection.RIGID
