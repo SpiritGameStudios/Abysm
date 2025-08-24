a0c527cad7ca0290ae4892fdb6b1896cc8662c82
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2025-08-24 15:49:25 +1000

    tweak(render): flipray rotation

--- src/client/java/dev/spiritstudios/abysm/client/render/entity/renderer/FliprayEntityRenderer.java
+++ src/client/java/dev/spiritstudios/abysm/client/render/entity/renderer/FliprayEntityRenderer.java
@@ -1,20 +1,19 @@
 package dev.spiritstudios.abysm.client.render.entity.renderer;
 
 import dev.spiritstudios.abysm.Abysm;
-import dev.spiritstudios.abysm.client.render.GeoUtil;
 import dev.spiritstudios.abysm.entity.ReticulatedFliprayEntity;
-import dev.spiritstudios.abysm.entity.depths.MysteriousBlobEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.RotationAxis;
-import software.bernie.geckolib.constant.dataticket.DataTicket;
+import net.minecraft.util.math.MathHelper;
+import software.bernie.geckolib.animatable.processing.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
+import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
+import software.bernie.geckolib.renderer.GeoEntityRenderer;
 import software.bernie.geckolib.renderer.base.GeoRenderState;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
-public class FliprayEntityRenderer<R extends LivingEntityRenderState & GeoRenderState> extends AbstractFishEntityRenderer<ReticulatedFliprayEntity, R> {
+public class FliprayEntityRenderer<R extends LivingEntityRenderState & GeoRenderState> extends GeoEntityRenderer<ReticulatedFliprayEntity, R> {
 
 	public FliprayEntityRenderer(EntityRendererFactory.Context context) {
 		super(context, new EntityModel());
@@ -26,19 +25,23 @@ public class FliprayEntityRenderer<R extends LivingEntityRenderState & GeoRender
 		});
 	}
 
-	@Override
-	protected void applyRotations(R renderState, MatrixStack matrixStack, float nativeScale) {
-		super.applyRotations(renderState, matrixStack, nativeScale);
-	}
-
-	@Override
-	protected boolean shouldApplyFishLandTransforms() {
-		return false;
-	}
-
 	public static class EntityModel extends DefaultedEntityGeoModel<ReticulatedFliprayEntity> {
 		public EntityModel() {
 			super(Abysm.id("reticulated_flipray"), false);
+		}
+
+		@Override
+		public void setCustomAnimations(AnimationState<ReticulatedFliprayEntity> animationState) {
+			GeoBone body = getAnimationProcessor().getBone("body");
+
+
+			if (body != null) {
+				float pitch = animationState.getData(DataTickets.ENTITY_PITCH);
+				float yaw = animationState.getData(DataTickets.ENTITY_YAW);
+
+				body.setRotX(-pitch * MathHelper.RADIANS_PER_DEGREE);
+				body.setRotY(-yaw * MathHelper.RADIANS_PER_DEGREE);
+			}
 		}
 	}
 }
